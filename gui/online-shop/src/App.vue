cf30bfe9758b3cfe1a60f9bea0acbdc045e46142
MrGoblings <mrgoblings@gmail.com>
2024-10-28 23:56:41 +0200

    add: simple navbar header

--- gui/online-shop/src/App.vue
+++ gui/online-shop/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import Header from './components/Header.vue';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <Header title="Online Shop" />
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -11,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="test" />
 </template>
 
 <style scoped>
---
+++ gui/online-shop/src/components/Header.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+    import '@fortawesome/fontawesome-free/css/all.min.css'
+    defineProps<{ title: string }>()
+
+    function test() {
+        console.log('test')
+    }
+</script>
+
+<template>
+    <header class="header">
+        <h1>{{ title }}</h1>
+        <nav class="nav-links">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/products/">Products</a></li>
+                <li><a href="/about/">About</a></li>
+                <li><a href="/contacts/">Contact</a></li>
+            </ul>
+            
+        </nav>
+        <nav class="nav-icons">
+            <ul>
+                <li><a @click="test"><i class="fas fa-search"></i></a></li>
+                <li><a href="/cart/"><i class="fas fa-shopping-cart"></i></a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+.header {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 3rem;
+    background-color: #f8f9fa;
+}
+
+.header h1 {
+    margin: 0;
+    color: #007bff;
+    font-size: 1.5rem;
+}
+
+.header nav {
+    display: flex;
+    gap: 1rem;
+    justify-content: space-between;
+}
+
+.header nav ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+    padding: 0;
+    margin: 0;
+}
+
+.header nav ul li a {
+    text-decoration: none;
+    color: #007bff;
+}
+</style>
