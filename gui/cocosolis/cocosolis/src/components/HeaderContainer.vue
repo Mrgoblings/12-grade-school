448e6a8aa8509060a36953f9b68746d5bdac39f7
MrGoblings <mrgoblings@gmail.com>
2024-11-11 14:17:50 +0200

    add: animation on header

--- gui/cocosolis/cocosolis/src/components/HeaderContainer.vue
+++ gui/cocosolis/cocosolis/src/components/HeaderContainer.vue
@@ -10,13 +10,27 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
         </div>
 
         <ul id="menu-cart" class="text-secondary-background">
-            <li><a href="#">Начало</a></li>
-            <li><a href="#">Продукти</a></li>
-            <li><a href="#">Club</a></li>
-            <li><a href="#">За Нас</a></li>
-            <li><a href="#">Контакти</a></li>
-            <li>
+            <li><a href="#" class="hover:text-secondary-accent">Начало</a></li>
+            <li><a href="#" class="hover:text-secondary-accent">Продукти</a></li>
+            <li><a href="#" class="hover:text-secondary-accent">Club</a></li>
+            <li><a href="#" class="hover:text-secondary-accent">За Нас</a></li>
+            <li><a href="#" class="hover:text-secondary-accent">Контакти</a></li>
+            <li class="text-xl">
                 <font-awesome-icon :icon="['fas', 'bars']" />
+            </li>
+
+            <li class="text-xl pl-3">
+                <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+            </li>
+            <li class="text-xl">
+                <font-awesome-icon :icon="['fas', 'cart-shopping']" />
+                <div id="circle-notification" class="bg-secondary-accent w-5 h-5 rounded-xl text-primary-foreground">0</div>
+            </li>
+            <li class="text-xl">
+                <a href="#" class="hover:text-secondary-accent">
+                    <font-awesome-icon :icon="['fas', 'user']" />
+                    Влез в профил
+                </a>   
             </li>
         </ul>
     </nav>
@@ -43,11 +57,29 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 #header-container #menu-cart {
     display: flex;
     justify-content: center;
+    text-align: center;
     padding: 0.5rem 0;
+    font-size: 1rem;
+    font-weight: 300;
 }
 
 #header-container #menu-cart li {
     margin: 0 1rem;
     text-transform: uppercase;
 }
+
+#circle-notification {
+    position: relative;
+    top: -35px;
+    right: -15px;
+    font-size: 0.8rem;
+    line-height: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-weight: 300;
+    font-size: 11px;
+}
+
 </style>
--- gui/cocosolis/cocosolis/src/components/HeaderTop.vue
+++ gui/cocosolis/cocosolis/src/components/HeaderTop.vue
@@ -3,13 +3,14 @@
 
 <template>
     <div id="container-top" class="bg-primary-background text-primary-foreground">
-        Пакетите за Black Friday са тук 🤍 Пазарувай с до 30% отстъпка                        
+        <p>Пакетите за Black Friday са тук 🤍 Пазарувай с до 30% отстъпка</p>
+        <p>Пакетите за Black Friday са тук 🤍 Пазарувай с до 30% отстъпка</p>
     </div>
 </template>
 
 <style scoped>
 #container-top {
-    width: 100%;
+    /* width: 100%; */
     text-align: center;
     font-size: 15px;
     font-weight: 500;
@@ -17,5 +18,29 @@
     max-height: 32px;
     padding-top: 0.35rem;
     padding-bottom: 0.35rem;
+
+    white-space: nowrap;
+    overflow: hidden;
+}
+
+#container-top p {
+    display: inline-block;
+    /* TODO: chech */
+    width: 100%;
+    padding-right: 0.35rem;
+    animation: scrollText 15s linear infinite;
+}
+
+#container-top:hover p {
+    animation-play-state: paused;
+}
+
+@keyframes scrollText {
+    0% {
+        transform: translateX(0);
+    }
+    100% {
+        transform: translateX(-100%);
+    }
 }
 </style>
